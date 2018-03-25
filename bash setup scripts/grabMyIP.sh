86fa7e003e6002e6b74c731efab8fd908e6e4fae
Katherine Dix <kathdix@seas.upenn.edu>
2018-03-25 18:39:56 +0000

    fixed script

--- bash setup scripts/grabMyIP.sh
+++ bash setup scripts/grabMyIP.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #grabs the ipv4 address of the specified interface (wlan1 in this case)
-echo $(ifconfig wlan1 | grep 'inet ' | cut -d ' ' -f 2)
+echo $(ifconfig wlan1 | grep 'inet ' | awk '{print $2}')
